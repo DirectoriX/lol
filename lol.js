a4ecbe942df401d2c9aa193968accb3675e6c306
DirectoriX <dontsov.mih@yandex.ru>
2015-12-10 19:01:49 +0300

    Update lol.js

--- lol.js
+++ lol.js
@@ -2,6 +2,8 @@
 $(".dixaba-statistics").remove();
 var s = document.createElement('div');
 s.className = "dixaba-statistics";
-s.innerHTML = "<div style='z-index: 1000; display: block; font-size: 20px; color: #660000; background-color: #ffffff; margin: 35px; padding: 5px; position: fixed; border: #000000; border-radius: 5px; border-style: solid'><b>"+$(".game-summary").length+" игр<br>"+$(".game-summary-victory").length+" побед<br>"+$(".game-summary-defeat").length+" поражений<br>Винрейт: "+($(".game-summary-victory").length/$(".game-summary").length*100).toPrecision(4)+"%</b></div>";
+var win = $(".game-summary-victory").length;
+var lose = $(".game-summary-defeat").length;
+s.innerHTML = "<div style='z-index: 1000; display: block; font-size: 20px; color: #660000; background-color: #ffffff; margin: 35px; padding: 5px; position: fixed; border: #000000; border-radius: 5px; border-style: solid'><b>"+(win+lose)+" игр<br>"+win+" побед<br>"+lose+" поражений<br>Винрейт: "+(win/(win+lose)*100).toPrecision(4)+"%</b></div>";
 document.body.insertBefore(s, document.body.firstChild);
 }).call(this);
